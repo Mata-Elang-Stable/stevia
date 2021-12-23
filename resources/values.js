34dc59a7a466937741f5a3afe69a3f327610115b
Ubuntu <ubuntu@ip-172-16-2-120.ap-northeast-1.compute.internal>
2021-12-24 01:58:14 +0700

    Add minor modifications

--- resources/values.js
+++ resources/values.js
@@ -4,20 +4,20 @@ const ip = {
 }
 
 const values = {
-  port : "1228",
+  port: 1228,
   enableSSH: false,
-  mongoUrl : `mongodb://MONGOUSER:MONGOPASS@172.16.2.30:27017/`,
-  mongoDbName : "stevia",
-  mongoCollection : "event1s",
-  //pgUrl = `postgresql://MONGOUSER:MONGOPASS@172.16.2.30:3211/mydb`,
+  mongoUrl: `mongodb://admin:jarkoM@172.16.2.50:27017/`,
+  mongoDbName: "stevia",
+  mongoCollection: "event1s",
+  pgUrl: `postgresql://dbuser:secretpassword@172.16.2.80:3211/mydb`,
   sshConf: {
-    username:'SSHUSER',
-    password:'SSHPASS',
-    host:publicIP,
-    port:22,
-    dstHost:publicIP,
-    dstPort:27017,
-    localHost:'127.0.0.1',
+    username: "sshUser",
+    password: "secretpassword",
+    host: ip.external,
+    port: 22,
+    dstHost: ip.external,
+    dstPort: 27017,
+    localHost: "127.0.0.1",
     localPort: 27000
   }
 };
